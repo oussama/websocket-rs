8698cf99aafb8fa529f1b12cd1b9cd3cda7d92e7
Oussama <md.oussama@gmail.com>
2018-02-09 17:05:43 +0100

    handle binary messages

--- src/stdw.rs
+++ src/stdw.rs
@@ -6,7 +6,8 @@ use std::sync::mpsc;
 use stdweb::web::WebSocket;
 use stdweb::web::IEventTarget;
 use stdweb::web::event::*;
-
+use stdweb::web::*;
+use stdweb::web::TypedArray;
 
 pub struct WebSocketSender {
     ws: WebSocket,
@@ -35,9 +36,36 @@ impl WebSocketSender {
 
         let sender = tx.clone();
         ws.add_event_listener(move |event: SocketMessageEvent| {
-            sender.send(SocketEvent::Message(SocketMessage::Text(
-                event.data().into_text().unwrap(),
-            ))).unwrap();
+            let data = if let Some(text) = event.data().into_text() {
+                SocketMessage::Text(text)
+            }else if let Some(blob) = event.data().into_blob(){
+                let s2 = sender.clone();
+                js!{
+                    console.log("blob");
+                    function loadBlob(callback){
+                        var reader = new FileReader();
+                        reader.addEventListener("loadend", function() {
+                            callback(reader.result);
+                        // reader.result contains the contents of blob as a typed array
+                        });
+                        reader.readAsArrayBuffer(@{blob});
+                    }
+
+                    loadBlob(@{move |buffer:ArrayBuffer|{
+                        let typed_array: TypedArray< u8 > = buffer.into();
+                        let data = SocketMessage::Binary(typed_array.to_vec());
+                        s2.send(SocketEvent::Message(data)).unwrap();
+                    }});
+                }
+                return;
+                //SocketMessage::Binary(blob.to_vec())
+            }else if let Some(buffer) = event.data().into_array_buffer() {
+                let typed_array: TypedArray< u8 > = buffer.into();
+                SocketMessage::Binary(typed_array.to_vec())
+            }else{
+                panic!("unknown type");
+            };
+            sender.send(SocketEvent::Message(data)).unwrap();
         });
 
         (WebSocketSender { ws }, rx)
